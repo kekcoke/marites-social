4d758fa91104e31c5a49afcc379e20dfb362c6e5
Kevin <kolioane@gmail.com>
2025-12-15 09:52:48 -0800

    Create Post model. Convert to Post dict inside response

--- src/app/main.py
+++ src/app/main.py
@@ -1,5 +1,7 @@
-from fastapi import FastAPI
-from fastapi.params import Body
+from fastapi import FastAPI, Body
+from pydantic import BaseModel
+from typing import Optional
+from models import Post
 
 app = FastAPI()
 
@@ -13,8 +15,7 @@ def get_posts():
     return {"data": "This is your posts"}
 
 @app.post("/createposts")
-def create_posts(payload: dict = Body(...)):
-    print(payload)
-    return {
-        "new_post": f"title: {payload['title']} content: {payload['content']}"
-    }
+def create_posts(new_post: Post):
+    print(new_post)
+    print(new_post.dict())
+    return {"data": new_post.dict()}
