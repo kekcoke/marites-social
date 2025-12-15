7022cb12213afc813ec990502c82127372c852e1
Kevin <kolioane@gmail.com>
2025-12-15 13:48:04 -0800

    add Post arr + Post helper + response handling on None post

--- src/app/main.py
+++ src/app/main.py
@@ -1,9 +1,24 @@
-from fastapi import FastAPI, Body
+from fastapi import FastAPI, Body, Response
 from pydantic import BaseModel
 from typing import Optional
 from models.post import Post
 
 app = FastAPI()
+
+posts = [{
+    "id": "1",
+    "title": "First Post",
+    "content": "Content of the first post",
+    "published": True,
+    "author": "Alice"
+},
+{
+    "id": "2",
+    "title": "Second Post",
+    "content": "Content of the second post",
+    "published": False,
+    "author": "Bob"
+}]
 
 @app.get("/")
 def root():
@@ -15,13 +30,22 @@ def create_posts(new_post: Post):
     print(new_post.dict())
     return {"data": new_post.dict()}
 
+@app.get("/posts/latest")
+def get_latest_post():
+    return {"data": "This is your latest post"}
+
 @app.get("/posts")
 def get_posts():
     return {"data": "This is your posts"}
 
 @app.get("/posts/{id}")
-def get_post(id: str):
-    return {"data": f"This is your post with id: {id}"}
+def get_post(id: str, response: Response):
+    post = find_post(id)
+    if not post:
+        response.status_code = 404
+        return {"post_detail": post}
+    
+    return {"post_detail": post}
 
 @app.put("/posts/{id}")
 def update_post(id: str, updated_post: Post = Body(...)):
@@ -30,3 +54,9 @@ def update_post(id: str, updated_post: Post = Body(...)):
 @app.delete("/posts/{id}")
 def delete_post(id: str):
     return {"data": f"Post with id: {id} has been deleted"}
+
+def find_post(id: str):
+    for post in posts:
+        if post["id"] == id:
+            return post
+    return None
