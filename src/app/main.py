a094699ab689fa64474fdfc74fa9a5b6dd25ae29
Kevin <kolioane@gmail.com>
2025-12-15 09:41:43 -0800

    POST: Create Posts

--- src/app/main.py
+++ src/app/main.py
@@ -11,3 +11,7 @@ def root():
 @app.get("/posts")
 def get_posts():
     return {"data": "This is your posts"}
+
+@app.post("/createposts")
+def create_posts():
+    return {"message": "Post created successfully"}
