864aece32908ec18311b5bc8cf92eb1f286235de
Kevin <kolioane@gmail.com>
2025-12-15 11:52:07 -0800

    init PUT, DELETE, GET, POST posts endpoints'

--- src/app/main.py
+++ src/app/main.py
@@ -5,17 +5,28 @@ from models import Post
 
 app = FastAPI()
 
-
 @app.get("/")
 def root():
     return {"message": "welcome to my api"}
-
-@app.get("/posts")
-def get_posts():
-    return {"data": "This is your posts"}
 
 @app.post("/createposts")
 def create_posts(new_post: Post):
     print(new_post)
     print(new_post.dict())
     return {"data": new_post.dict()}
+
+@app.get("/posts")
+def get_posts():
+    return {"data": "This is your posts"}
+
+@app.get("/posts/{id}")
+def get_post(id: str):
+    return {"data": f"This is your post with id: {id}"}
+
+@app.put("/posts/{id}")
+def update_post(id: str, updated_post: Post = Body(...)):
+    return {"data": f"Post with id: {id} has been updated"}
+
+@app.delete("/posts/{id}")
+def delete_post(id: str):
+    return {"data": f"Post with id: {id} has been deleted"}
