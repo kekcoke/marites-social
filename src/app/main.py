23080455608b417c53c9056a01333f79f168488f
Kevin <kolioane@gmail.com>
2025-12-17 12:02:54 -0800

    use session to GET/POST post endpoints

--- src/app/main.py
+++ src/app/main.py
@@ -8,6 +8,7 @@ from fastapi.params import Body
 from pydantic import BaseModel
 from random import randrange
 from typing import Optional
+from db.session import get_db_session
 from models.post import Post
 
 # Load environment variables from .env file
@@ -31,45 +32,21 @@ logger.info("Logging is configured.")
 # Initialize FastAPI app
 app = FastAPI()
 
-def get_db_connection():
-    """Establish a connection to the database using environment variables.
-    """
-    try:
-        conn = psycopg2.connect(
-            host=os.getenv("DB_HOST"),
-            database=os.getenv("DB_NAME"),
-            user=os.getenv("DB_USERNAME"),
-            password=os.getenv("DB_PASSWORD"),
-            port=os.getenv("DB_PORT"),
-            connect_timeout=os.getenv("CONNECT_TIMEOUT"),
-            sslmode=os.getenv("SSL_MODE"),
-            cursor_factory=psycopg2.extras.RealDictCursor
-        )
-        return conn
-    except Exception as e:
-        logger.info(f"Error connecting to the database: {e}")
-        return HTTPException(status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
-                             detail="Database connection error")
-
 @app.get("/db-test")
 def db_test():
     """Test database connection and return a success message if connected.
     """
-    conn = get_db_connection()
-    if isinstance(conn, HTTPException):
-        raise conn
-    cursor = conn.cursor()
-    cursor.execute("SELECT 1;")
-    result = cursor.fetchone()
-    cursor.close()
-    conn.close()
-    if result and result[0] == 1:
-        return {"message": "Database connection successful!"}
-    else:
-        raise HTTPException(status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
-                            detail="Database test query failed")
-
-posts: list[Post] = []
+    with get_db_session() as conn:
+        with conn.cursor() as cursor:
+            cursor.execute("SELECT 1;")
+            result = cursor.fetchone()
+            if result and result[0] == 1:
+                return {"message": "Database connection successful"}
+            else:
+                raise HTTPException(
+                    status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
+                    detail="Database connection test failed"
+                )
 
 @app.get("/")
 def root():
@@ -77,40 +54,56 @@ def root():
 
 @app.post("/createposts", status_code=status.HTTP_201_CREATED)
 def create_posts(new_post: Post):
-    post_dict = new_post.dict()
-    post_dict['id'] = range(1, 1000000)  # Simulating ID assignment
-    posts.append(post_dict)
-    return {"data": post_dict}
+    """Create a new post in the database.
+    """
+    with get_db_session() as conn:
+        cursor = conn.cursor()
+        cursor.execute(
+            "INSERT INTO posts (title, content, published) VALUES (%s, %s, %s) RETURNING *;",
+            (new_post.title, new_post.content, new_post.published))
+        post = cursor.fetchone()
+        conn.commit()
+        return {"data": dict(post)}
+
 
 @app.get("/posts/latest")
 def get_latest_post():
-    if not posts:
-        raise HTTPException(
-            status_code=status.HTTP_404_NOT_FOUND,
-            detail="No posts available"
-        )
-    latest_post = posts[-1]
-    return {"latest_post": latest_post}
+    """Get the most recently created post from the database."""
+    with get_db_session() as conn:
+        cursor = conn.cursor()
+        cursor.execute("SELECT * FROM posts ORDER BY created_at DESC LIMIT 1;")
+        post = cursor.fetchone()
+        if not post:
+            raise HTTPException(
+                status_code=status.HTTP_404_NOT_FOUND,
+                detail="No posts available"
+            )
+        return {"latest_post": dict(post)}
     
-
 @app.get("/posts")
 def get_posts():
-    if not posts or len(posts) == 0:
-        raise HTTPException(
-            status_code=status.HTTP_404_NOT_FOUND,
-            detail="No posts available"
-        )
-    return {"data": posts}
+    """Get all posts from the database."""
+    posts = []
+    with get_db_session() as conn:
+            cursor = conn.cursor()
+            cursor.execute("SELECT * FROM posts;")
+            posts = cursor.fetchall()
+            return {"data": posts}
 
 @app.get("/posts/{id}")
 def get_post(id: int, response: Response):
-    post = find_post(id)
-    if not post:
-        raise HTTPException(
-            status_code=status.HTTP_404_NOT_FOUND,
-            detail=f"Post with id: {id} was not found"
-        )
-    return {"post_detail": post}
+    """Get a specific post by ID from the database."""
+    with get_db_session() as conn:
+            cursor = conn.cursor()
+            cursor.execute("SELECT * FROM posts WHERE id = %s", (id,))
+            post = cursor.fetchone()
+            
+            if not post:
+                raise HTTPException(
+                    status_code=status.HTTP_404_NOT_FOUND,
+                    detail=f"Post with id: {id} was not found"
+                )
+            return {"data": post}
 
 @app.put("/posts/{id}")
 def update_post(id: int, post: Post):
