9bf38f74a0a48f58f79c273d5a4e665095db8f82
Kevin <kolioane@gmail.com>
2026-01-05 16:35:06 -0800

    Omit index. PK's already indexed by default

--- src/app/models/post.py
+++ src/app/models/post.py
@@ -15,7 +15,7 @@ Base = declarative_base()
 class Post(Base):
     __tablename__ = "posts"
 
-    id = Column(Integer, primary_key=True, index=True)
+    id = Column(Integer, primary_key=True)
     title = Column(String, nullable=False)
     content = Column(Text, nullable=False)
     published = Column(Boolean, default=True)
