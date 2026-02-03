8ba41818a94969d2d141e9d348fd0f50fa2e6246
Kevin <kolioane@gmail.com>
2026-02-02 20:11:09 -0800

    Add Token + TokenData to __init__.py

--- src/app/schemas/__init__.py
+++ src/app/schemas/__init__.py
@@ -1,6 +1,6 @@
 from .post import Post, CreatePost, UpdatePost, PostResponse
 from .user import UserCreate, UserResponse, UserLogin, User
-from .token import Token
+from .token import Token, TokenData
 __all__ = [
     "Post", 
     "CreatePost", 
@@ -10,5 +10,6 @@ __all__ = [
     "UserLogin",
     "UserResponse",
     "User",
-    "Token"
+    "Token",
+    "TokenData"
 ]
