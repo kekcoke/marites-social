b2d7c1bdd443bca7c27d936044f7b93f5bd5862f
Kevin <kolioane@gmail.com>
2026-02-06 10:54:22 -0800

    add user_id FK & constraint to models

--- src/app/models/post.py
+++ src/app/models/post.py
@@ -1,14 +1,16 @@
 from sqlalchemy import (
     Column,
+    ForeignKey,
     String,
     Boolean,
     DateTime,
     Float,
     Integer,
     Text,
-    func,
-    text
+    func
 )
+from sqlalchemy.dialects.postgresql import UUID
+
 from src.app.db.connection import Base
 
 class Post(Base):
@@ -30,6 +32,12 @@ class Post(Base):
         server_default=func.now(),
         onupdate=func.now(), #auto-update timestamp on modification
         nullable=False,
+    )
+    user_id = Column(
+        UUID(as_uuid=True), 
+        ForeignKey("users.id", 
+        ondelete="CASCADE"),
+        nullable=False
     )
     rating = Column(Float, nullable=True)
     likes = Column(Integer, server_default="0", nullable=False)
