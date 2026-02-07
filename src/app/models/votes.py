346ea8bda1cc07e58992de3d8625db5fad8c7657
Kevin <kolioane@gmail.com>
2026-02-06 19:52:28 -0800

    Coerce user_id in Vote as uuid

--- src/app/models/votes.py
+++ src/app/models/votes.py
@@ -8,5 +8,5 @@ from src.app.db.connection import Base
 
 class Vote(Base):
     __tablename__ = "votes"
-    user_id = Column(UUID, ForeignKey("users.id", ondelete="CASCADE"), primary_key=True)
+    user_id = Column(UUID(as_uuid=True), ForeignKey("users.id", ondelete="CASCADE"), primary_key=True)
     post_id = Column(Integer, ForeignKey("posts.id", ondelete="CASCADE"), primary_key=True)
