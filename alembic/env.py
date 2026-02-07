eaec09ced52ac9b4e04dc2fcb97aab95d1fefe9f
Kevin <kolioane@gmail.com>
2026-02-06 21:05:40 -0800

    import models to alembic env.py

--- alembic/env.py
+++ alembic/env.py
@@ -10,6 +10,9 @@ from sqlalchemy import engine_from_config, pool
 from alembic import context
 from src.app import Base, config as c
 
+# Import models
+from src.app.models import *
+
 # this is the Alembic Config object, which provides
 # access to the values within the .ini file in use.
 config = context.config
