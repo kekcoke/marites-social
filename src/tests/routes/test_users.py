dee789d1622c67f66b0ecff0c49e21d898977f24
Kevin <kolioane@gmail.com>
2026-02-07 21:32:26 -0800

    test_create_user

--- src/tests/routes/test_users.py
+++ src/tests/routes/test_users.py
@@ -2,16 +2,22 @@ import faker
 import random
 from fastapi.testclient import TestClient
 from app.main import app
+from app import schemas
 
 client = TestClient(app)
 fake = faker.Faker()
 
 def test_create_user():
+    fname = fake.first_name()
+    lname = fake.last_name()
+    username = f"{fname}.{lname}"
+    email = f"{username}@email.com"
     password = fake.password()
-    res = client.post("/users", json={
-        "email": f"{fake.first_name()}.{fake.last_name()}@email.com",
+    res = client.post("/users/", json={
+        "username": username,
+        "email" : email,
         "password" : password
     })
+    new_user = schemas.UserResponse(**res.json())
+    assert new_user.email == email
     assert res.status_code == 201
-
-    
