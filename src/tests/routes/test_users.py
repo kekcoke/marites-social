7feb5949de27c13b4cf2e74163c64e31414f730e
Kevin <kolioane@gmail.com>
2026-02-09 00:06:45 -0800

    refactor test_users

--- src/tests/routes/test_users.py
+++ src/tests/routes/test_users.py
@@ -1,20 +1,46 @@
-import faker
-import random
+import pytest
+from faker import Faker
 from app import schemas
 
 fake = faker.Faker()
 
-def test_create_user():
+@pytest.fixture
+def user_payload():
     fname = fake.first_name()
     lname = fake.last_name()
     username = f"{fname}.{lname}"
-    email = f"{username}@email.com"
-    password = fake.password()
-    res = client.post("/users/", json={
+
+    return {
         "username": username,
-        "email" : email,
-        "password" : password
-    })
-    new_user = schemas.UserResponse(**res.json())
-    assert new_user.email == email
+        "email": f"{username}@email.com",
+        "password" : fake.password(),
+    }
+
+@pytest.fixture
+def created_user(client, user_payload):
+    res = client.post("/users/", json=user_payload)
+
     assert res.status_code == 201
+
+    user = schemas.UserResponse(**res.json())
+
+    return {
+        "user": user,
+        "password" : user_payload["password"],
+    }
+
+def test_create_user(created_user):
+    user = created_user["user"]
+    assert user.email is not None
+
+
+def test_login_user(client, created_user):
+    res = client.post(
+        "/login",
+        data={
+            "username": created_user["user"].email,
+            "password": created_user["password"],
+        },
+    )
+
+    assert res.status_code == 200
