dd2cf36ee9f5e09082e60fd2e662ba6828feaa28
Kevin <kolioane@gmail.com>
2026-02-08 17:50:43 -0800

    Init db_test_setup

---
+++ src/tests/db_test_setup.py
@@ -0,0 +1,27 @@
+from fastapi.testclient import TestClient
+from app.db import get_db_session, Base
+from app.config import get_config
+from app.main import app
+from app import schemas
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+
+SQLALCHEMY_DB_URL = f'{get_config().db_database_url}_test'
+
+engine = create_engine(SQLALCHEMY_DB_URL)
+
+TestingSessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+Base.metadata.create_all(bind=engine)
+
+
+def override_get_db_test():
+    db = TestingSessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
+
+# Replace db with test on app
+app.dependencies_overrides[get_db_session] = override_get_db_test()
+
+client = TestClient(app)
