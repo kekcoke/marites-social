50e52bf3ca27d400a77748fca6b3a89fb1826bdb
Kevin <kolioane@gmail.com>
2026-02-15 12:27:18 -0800

    Update env.py import + migration: extensions, timescaledb ommitted

--- alembic/env.py
+++ alembic/env.py
@@ -12,9 +12,9 @@ from app.db import Base
 from app.config import get_config
 
 # Import models
-from src.app.models import *
+import app.models 
 
-
+# Assign config 
 c = get_config()
 
 # this is the Alembic Config object, which provides
@@ -35,6 +35,8 @@ if config.config_file_name is not None:
 # from myapp import mymodel
 # target_metadata = mymodel.Base.metadata
 target_metadata = Base.metadata
+# print("Tables:", Base.metadata.tables.keys())
+
 
 # other values from the config, defined by the needs of env.py,
 # can be acquired:
